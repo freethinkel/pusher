1e52e47e3e9089c0b9f90a06c56094d261d5e347
Егор <freethinkel@gmail.com>
2021-02-01 10:58:12 +0300

    try fix build

--- index.ts
+++ index.ts
@@ -1,22 +1,23 @@
 import express from 'express';
 import Telegraf from 'telegraf';
-import { config } from './src/helpers/config';
+import {config} from './src/helpers/config';
 import appDb from './src/models';
-import { startModule } from './src/modules/start';
+import {startModule} from "./src/modules/start";
 import tokenRouter from './src/modules/token-router';
 
 const main = async () => {
-	await appDb.connect();
-	const bot = new Telegraf(config.botToken || '');
-	bot.start(startModule);
-	await bot.launch();
+    await appDb.connect();
+    const bot = new Telegraf(config.botToken || '');
+    bot.start(startModule);
+    await bot.launch();
 
-	const app = express();
-	app.use('/send', tokenRouter(bot));
-	const PORT = process.env.PORT || 3000;
-	app.listen(PORT, () => {
-		console.log(`HTTP server listening on ${PORT}`);
-	});
+    const app = express();
+    app.use('/send', tokenRouter(bot));
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`HTTP server listening on ${PORT}`);
+    });
 };
 
-main().then(_ => {});
+main().then(_ => {
+});
